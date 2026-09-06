6d713e0693faf25b070000391c39ae3dc35bc570
jettwang <jamiesun.net@gmail.com>
2026-09-07 00:55:26 +0800

    fix(imagine): install binary using file check instead of executable check

--- Formula/imagine.rb
+++ Formula/imagine.rb
@@ -25,7 +25,7 @@ class Imagine < Formula
   end
 
   def install
-    bin.install Dir["imagine-*"].find { |f| File.file?(f) && File.executable?(f) } => "imagine"
+    bin.install Dir["imagine-*"].find { |f| File.file?(f) && true } => "imagine"
     pkgshare.install "skills" if File.directory?("skills")
   end
 
